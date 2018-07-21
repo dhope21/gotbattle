579322a7eabef94a35a4b460f8be24308c5ddc29
abhijeet dhope <dhope21@gmail.com>
2018-07-21 20:22:15 +0530

    added jwt token for authenticati

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://dhope21:abhijeet21@ds147391.mlab.com:47391/gotbattle'
+    
+};
+// export MONGOLAB_URI="mongodb://dhope21:abhijeet21@ds147391.mlab.com:47391/gotbattle"
--- server.js
+++ server.js
@@ -1,20 +1,57 @@
 var express = require('express');
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 var Battle = require('./model');
-
-const Controller = require('./controller');
+var config = require('./config'); 
+var Controller = require('./controller');
 var _ = require('lodash');
-
-// export MONGOLAB_URI="mongodb://dhope21:abhijeet21@ds147391.mlab.com:47391/gotbattle"
+var jwt    = require('jsonwebtoken');
+var port = process.env.PORT || 3000
+var User = require('./user');
 // mongoose connection
-mongoose.connect(process.env.MONGOLAB_URI, function (error) {
-  if (error) console.error(error);
-  else console.log('mongo connected');
-});
+mongoose.connect(config.database);
 
 const app = express();
+app.set('superSecret', config.secret);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+
+  }
+});
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+
 
 // returns the list of all battels
 app.get('/list', (req, res) => {
@@ -42,7 +79,75 @@ app.get('/search', (req, res) => {
 
 app.get('/stats', Controller.getStats);
 
-app.listen(3000, () => console.log('listening on port 3000!'));
+
+
+app.get('/createUser', function(req, res) {
+  // create a sample user
+  var newUser = new User({ 
+    name: 'Arya Stark', 
+    password: 'valarmorghulis',
+    admin: true 
+  });
+
+  // save the sample user
+  newUser.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+
+app.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});   
+
+
+
+app.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+      admin: user.admin 
+    };
+        var token = jwt.sign(payload, app.get('superSecret'));
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+
+app.listen(port, () => console.log('listening on port 3000!'));
 
 
 
